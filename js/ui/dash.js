d060c4683a51f03df245164defb98dd57c2c081f
guhao <davidguhao@icloud.com>
2022-08-16 20:57:30 +0800

    things about dash (debug)

--- js/ui/dash.js
+++ js/ui/dash.js
@@ -911,7 +911,7 @@ var Dash = GObject.registerClass({
                 });
             }
             this.generateDragPlaceHolderAndShow();
-            this._box.set_child_at_index(actor, this._dragPlaceholderPos);
+            this._box.set_child_at_index(actor.container, this._dragPlaceholderPos);
         }
 
         if (!this._dragPlaceholder)
